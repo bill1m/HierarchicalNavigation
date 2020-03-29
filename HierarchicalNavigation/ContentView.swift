ffaddbabd2065640c908f1841a8b2ffedbae8f49
Bill Morrison <bill1.morrison1@gmail.com>
2020-03-29 09:56:59 -0700

    Initial Commit

---
+++ HierarchicalNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HierarchicalNavigation
+//
+//  Created by Bill Morrison on 3/29/20.
+//  Copyright © 2020 Bill Morrison. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
